c1cbd94434ccc0c82db95ab76f01737144a03326
Edwin Granados  <edwin_go@hotmail.es>
2016-08-11 23:06:14 -0700

    Initial Commit

---
+++ PruebaGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaGitHub
+//
+//  Created by Edwin Osvaldo Granados Osornio on 10/08/16.
+//  Copyright © 2016 Granados. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
